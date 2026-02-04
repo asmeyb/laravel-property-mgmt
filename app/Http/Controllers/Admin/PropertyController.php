586b401c4613b7b1e0bf6c15151e7f11867c8281
asmeyb <asmeyb@gmail.com>
2026-02-04 16:01:07 +0300

    CRUD For Location

---
+++ app/Http/Controllers/Admin/PropertyController.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Time;
+use App\Models\Location;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+use Illuminate\Support\Facades\File;
+
+class PropertyController extends Controller
+{
+    public function AllTimes()
+    {
+        $times = Time::latest()->get();
+        return view('admin.backend.time.all_time', compact('times'));
+    }
+    //End Method 
+
+    public function AddTimes()
+    {
+        return view('admin.backend.time.add_time');
+    }
+    //End Method 
+
+    public function StoreTimes(Request $request)
+    {
+
+        Time::create([
+            'time_name' => $request->time_name,
+            'time_hour' => $request->time_hour,
+        ]);
+
+        $notification = array(
+            'message' => 'Time Added Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.times')->with($notification);
+
+    }
+    //End Method 
+
+    public function EditTimes($id)
+    {
+        $times = Time::find($id);
+        return view('admin.backend.time.edit_time', compact('times'));
+    }
+    //End Method 
+
+    public function UpdateTimes(Request $request)
+    {
+
+        $time_id = $request->id;
+
+        Time::find($time_id)->update([
+            'time_name' => $request->time_name,
+            'time_hour' => $request->time_hour,
+        ]);
+
+        $notification = array(
+            'message' => 'Time Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.times')->with($notification);
+
+    }
+    //End Method 
+
+    public function DeleteTimes($id)
+    {
+
+        Time::find($id)->delete();
+
+        $notification = array(
+            'message' => 'Time Deleted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+
+    }
+    //End Method 
+
+    ///////////// Location Method //////////////
+
+    public function AllLocation()
+    {
+        $location = Location::latest()->get();
+        return view('admin.backend.location.all_location', compact('location'));
+    }
+    //End Method 
+
+    public function AddLocation()
+    {
+        return view('admin.backend.location.add_location');
+    }
+    //End Method 
+
+    public function StoreLocation(Request $request)
+    {
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $manager = new ImageManager(new Driver());
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            $img = $manager->read($image);
+            $img->resize(300, 395)->save(public_path('upload/location/' . $name_gen));
+            $save_url = 'upload/location/' . $name_gen;
+        }
+
+        Location::create([
+            'name' => $request->name,
+            'image' => $save_url
+        ]);
+
+        $notification = array(
+            'message' => 'Location Added Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('all.location')->with($notification);
+
+    }
+    //End Method 
+
+    public function EditLocation($id)
+    {
+        $location = Location::find($id);
+        return view('admin.backend.location.edit_location', compact('location'));
+    }
+    //End Method 
+
+
+    public function UpdateLocation(Request $request)
+    {
+
+        $location_id = $request->id;
+        $location = Location::findOrFail($location_id);
+
+        if ($request->hasFile('image')) {
+
+            if (File::exists(public_path($location->image))) {
+                File::delete(public_path($location->image));
+            }
+
+            $image = $request->file('image');
+            $manager = new ImageManager(new Driver());
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            $img = $manager->read($image);
+            $img->resize(300, 395)->save(public_path('upload/location/' . $name_gen));
+            $save_url = 'upload/location/' . $name_gen;
+
+            Location::find($location_id)->update([
+                'name' => $request->name,
+                'image' => $save_url
+            ]);
+
+            $notification = array(
+                'message' => 'Location Added Successfully',
+                'alert-type' => 'success'
+            );
+            return redirect()->route('all.location')->with($notification);
+
+        } else {
+
+            Location::find($location_id)->update([
+                'name' => $request->name,
+            ]);
+
+            $notification = array(
+                'message' => 'Location Added Successfully',
+                'alert-type' => 'success'
+            );
+            return redirect()->route('all.location')->with($notification);
+        }
+
+    }
+    //End Method 
+
+    public function DeleteLocation($id)
+    {
+        $location = Location::findOrFail($id);
+
+        // Delete main image
+        if ($location->image && file_exists(public_path($location->image))) {
+            unlink(public_path($location->image));
+        }
+
+        Location::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'Location Deleted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+
+    }
+    //End Method 
+
+}
---
+++ routes/web.php
@@ -0,0 +1,71 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\IsUser;
+use App\Http\Controllers\Admin\PropertyController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/// Only User Role Access Started
+
+Route::middleware(['auth', IsUser::class])->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+});
+
+/// End User Role Access Started
+
+
+/// Only Admin Role Access Started
+
+Route::middleware(['auth', IsAdmin::class])->group(function () {
+
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/profile/update', [AdminController::class, 'AdminProfileUpdate'])->name('admin.profile.update');
+
+    Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
+    Route::post('/admin/password/update', [AdminController::class, 'AdminPasswordUpdate'])->name('admin.password.update');
+
+
+    Route::controller(PropertyController::class)->group(function () {
+        Route::get('/all/times', 'AllTimes')->name('all.times');
+        Route::get('/add/times', 'AddTimes')->name('add.times');
+        Route::post('/store/times', 'StoreTimes')->name('store.times');
+        Route::get('/edit/times/{id}', 'EditTimes')->name('edit.times');
+        Route::post('/update/times', 'UpdateTimes')->name('update.times');
+        Route::get('/delete/times/{id}', 'DeleteTimes')->name('delete.times');
+
+        // Location
+        Route::get('/all/location', 'AllLocation')->name('all.location');
+        Route::get('/add/location', 'AddLocation')->name('add.location');
+        Route::post('/store/location', 'StoreLocation')->name('store.location');
+        Route::get('/edit/location/{id}', 'EditLocation')->name('edit.location');
+        Route::post('/update/location', 'UpdateLocation')->name('update.location');
+        Route::get('/delete/location/{id}', 'DeleteLocation')->name('delete.location');
+
+    });
+
+
+});
+
+/// End Admin Role Access Started
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
